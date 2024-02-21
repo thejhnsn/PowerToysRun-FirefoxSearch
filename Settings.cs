48e9f95ab15c4cf922d0240fc8004b9971cf755f
Johnny Nguyen <johnny.nguyen@outlook.at>
2024-02-21 15:37:19 +0100

    fix solution

---
+++ Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace Community.Powertoys.Run.Plugin.FirefoxSearch.Properties
+{
+    public class Settings
+    {
+        public uint maxSearchResults { get; set; } = 40;
+        public bool searchBookmarks { get; set; } = true;
+        public bool searchHistory { get; set; } = true;
+    }
+}
